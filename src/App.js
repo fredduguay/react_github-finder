e90955f546af678e61b7fd32a67a58eafa2ec15e
Fred Duguay <fredduguay@users.noreply.github.com>
2022-05-02 17:57:42 -0400

    Home, about, 404 pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import NotFound from './components/pages/NotFound';
+
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
 
@@ -7,7 +11,14 @@ function App() {
     <Router>
       <div className='flex flex-col justify-between h-screen'>
         <Navbar />
-        <main className='container mx-auto pb-12'>Content</main>
+        <main className='container mx-auto pb-12'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/notfound' element={<NotFound />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
